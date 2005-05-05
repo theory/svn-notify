a0fe23384e97c95016a75426a7d90009ee724b97
David Wheeler <david@justatheory.com>
2005-05-05 05:29:43 +0000

    Incremented version number to 2.47.

--- lib/SVN/Notify.pm
+++ lib/SVN/Notify.pm
@@ -3,7 +3,7 @@ package SVN::Notify;
 # $Id$
 
 use strict;
-$SVN::Notify::VERSION = '2.46';
+$SVN::Notify::VERSION = '2.47';
 
 =begin comment
 
--- lib/SVN/Notify/HTML/ColorDiff.pm
+++ lib/SVN/Notify/HTML/ColorDiff.pm
@@ -6,7 +6,7 @@ use strict;
 use HTML::Entities;
 use SVN::Notify::HTML ();
 
-$SVN::Notify::HTML::ColorDiff::VERSION = '2.46';
+$SVN::Notify::HTML::ColorDiff::VERSION = '2.47';
 @SVN::Notify::HTML::ColorDiff::ISA = qw(SVN::Notify::HTML);
 
 =head1 Name
