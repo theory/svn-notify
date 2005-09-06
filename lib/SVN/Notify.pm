517dc546060cf47bb115d4f6864fa91b05520bb3
David Wheeler <david@justatheory.com>
2005-09-06 19:23:01 +0000

    Incremented version number to 2.49.

--- lib/SVN/Notify.pm
+++ lib/SVN/Notify.pm
@@ -3,7 +3,7 @@ package SVN::Notify;
 # $Id$
 
 use strict;
-$SVN::Notify::VERSION = '2.48';
+$SVN::Notify::VERSION = '2.49';
 
 =begin comment
 
--- lib/SVN/Notify/HTML.pm
+++ lib/SVN/Notify/HTML.pm
@@ -6,7 +6,7 @@ use strict;
 use HTML::Entities;
 use SVN::Notify ();
 
-$SVN::Notify::HTML::VERSION = '2.48';
+$SVN::Notify::HTML::VERSION = '2.49';
 @SVN::Notify::HTML::ISA = qw(SVN::Notify);
 
 __PACKAGE__->register_attributes(
