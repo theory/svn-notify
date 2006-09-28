3a6b7394525939dd5ed57a231d1a0f67d11033e0
David Wheeler <david@justatheory.com>
2006-09-28 18:46:00 +0000

    Incremented version number to 2.65.

--- lib/SVN/Notify/HTML.pm
+++ lib/SVN/Notify/HTML.pm
@@ -6,7 +6,7 @@ use strict;
 use HTML::Entities;
 use SVN::Notify ();
 
-$SVN::Notify::HTML::VERSION = '2.64';
+$SVN::Notify::HTML::VERSION = '2.65';
 @SVN::Notify::HTML::ISA = qw(SVN::Notify);
 
 __PACKAGE__->register_attributes(
--- lib/SVN/Notify/HTML/ColorDiff.pm
+++ lib/SVN/Notify/HTML/ColorDiff.pm
@@ -6,7 +6,7 @@ use strict;
 use HTML::Entities;
 use SVN::Notify::HTML ();
 
-$SVN::Notify::HTML::ColorDiff::VERSION = '2.64';
+$SVN::Notify::HTML::ColorDiff::VERSION = '2.65';
 @SVN::Notify::HTML::ColorDiff::ISA = qw(SVN::Notify::HTML);
 
 =head1 Name
