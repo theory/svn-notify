d93ba1809cbafe5660e3b414cae30bf21ae3d514
David Wheeler <david@justatheory.com>
2008-02-06 06:05:30 +0000

    Updated the copyright year.
    Still trying to get SVN::Notify to recognize separate paragraphs.
      
    'Know what I mean?

--- lib/SVN/Notify.pm
+++ lib/SVN/Notify.pm
@@ -2135,7 +2135,7 @@ David Wheeler <david@kineticode.com>
 
 =head1 Copyright and License
 
-Copyright (c) 2004-2007 Kineticode, Inc. All Rights Reserved.
+Copyright (c) 2004-2008 Kineticode, Inc. All Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it under the
 same terms as Perl itself.
