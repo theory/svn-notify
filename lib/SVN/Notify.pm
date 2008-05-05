b149e6998954bb0bb22e2106b5f45a7f4fda40d5
David Wheeler <david@justatheory.com>
2008-05-05 19:22:31 +0000

    Updated copyright.

--- lib/SVN/Notify.pm
+++ lib/SVN/Notify.pm
@@ -2466,7 +2466,7 @@ David E. Wheeler <david@kineticode.com>
 
 =head1 Copyright and License
 
-Copyright (c) 2004-2008 Kineticode, Inc. All Rights Reserved.
+Copyright (c) 2004-2008 Kineticode, Inc. Some Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
